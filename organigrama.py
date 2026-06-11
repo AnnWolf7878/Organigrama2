7d3dd3f119ebd183a0ee65e0547408b0a598435e
Andrea Mena L. <tu@email.com>
2026-06-11 16:32:14 -0600

    Corrección final de servidor

--- organigrama.py
+++ organigrama.py
@@ -1,18 +1,16 @@
 ﻿from flask import Flask, render_template, jsonify
+import os
 
 app = Flask(__name__)
 
-# Ruta principal
 @app.route('/')
 def home():
     return render_template('login.html')
 
-# Ruta del Dashboard
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
-# Ruta de la API
 @app.route('/api/organigrama')
 def get_organigrama():
     return jsonify({
@@ -22,4 +20,6 @@ def get_organigrama():
     })
 
 if __name__ == '__main__':
-    app.run()
+    # Render necesita usar la variable de entorno PORT si está disponible
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
